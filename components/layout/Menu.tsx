3fd321ec0b593806655d65101f339db32b014a0a
Matteo Courquin <matteo.courquin@gmail.com>
2026-02-08 13:30:22 +0100

    :bug: Fix linter

--- components/layout/Menu.tsx
+++ components/layout/Menu.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 import { useRef, useState } from 'react';
 import Language from '../shared/language';
 import NewsletterForm, { AnimatedNewsletterFormRef } from '../shared/newsletter-form';
-import Sound from '../shared/sound';
+import Sound from '../shared/soun';
 import Time from '../shared/time';
 import AnimatedLink from '../ui/animated-link';
 import Hint from '../ui/hint';
