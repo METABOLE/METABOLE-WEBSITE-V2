58ce058ad84c97be3ed7c69e401867387ba2185b
Matteo Courquin <matteo.courquin@gmail.com>
2026-02-23 21:52:42 +0100

    :bug: Fix all

--- features/about/team.tsx
+++ features/about/team.tsx
@@ -4,19 +4,23 @@ import Button from '@/components/ui/button';
 import Typography from '@/components/ui/typography';
 import { Team as TeamType } from '@/types';
 import CardTeam from './team/card-team';
+import { useLanguage } from '@/providers/language.provider';
 
 const Team = ({ team }: { team: TeamType[] }) => {
+  const { isFrench } = useLanguage();
   return (
     <section className="py-y-double-default px-x-default grid grid-cols-12 gap-5 overflow-hidden">
       <div className="pb-y-half-default col-span-12 flex flex-col justify-between sm:flex-row lg:col-span-5 lg:flex-col">
-        <Title color="blue">L’ÉQUIPE</Title>
+        <Title color="blue">{isFrench ? 'L’ÉQUIPE' : 'THE TEAM'}</Title>
         <div className="pt-y-half-default sm:pt-0">
           <Typography className="p3-regular max-w-sm pb-6" variant="p">
-            Travailler avec Metabole, c’est surtout savoir que l’on va faire du bon travail ensemble
-            avec des clients qui nous ressemblent. Ambitieux et bienveillants. Leur satisfaction est
-            notre premier objectif.
+            {isFrench
+              ? 'Travailler avec Metabole, c’est surtout savoir que l’on va faire du bon travail ensemble avec des clients qui nous ressemblent. Ambitieux et bienveillants. Leur satisfaction est notre premier objectif.'
+              : 'Working with Metabole means knowing we will do great work together, with clients who share our values. Ambitious and kind. Their satisfaction is our top priority.'}
           </Typography>
-          <Button color="secondary">CONTACTEZ-NOUS</Button>
+          <Button color="secondary" href="/contact">
+            {isFrench ? 'CONTACTEZ-NOUS' : 'CONTACT US'}
+          </Button>
         </div>
       </div>
       <HorizontalSmoothScroll className="pr-x-default team-container col-span-12 flex w-[calc(100%+var(--x-default))] shrink-0 lg:col-span-7">
--- features/contact/contact.tsx
+++ features/contact/contact.tsx
@@ -1,13 +1,15 @@
 import FullWidthTypo from '@/components/shared/full-width-typo';
-import { Data } from '@/types';
+import { BREAKPOINTS, Data } from '@/types';
 import ContactForm from './contact-form';
 import Scroller from './scroller';
 import Footer from './footer';
+import { useMatchMedia } from '@/hooks/useCheckScreenSize';
 
 const Contact = ({ data, totalAwards }: { data: Data; totalAwards: number }) => {
+  const isTablet = useMatchMedia(BREAKPOINTS.MD);
   return (
     <div className="px-x-default relative grid h-fit w-screen grid-cols-12 gap-5 sm:h-screen">
-      <Scroller />
+      {!isTablet && <Scroller />}
       <div className="py-y-double-default col-span-12 flex w-full flex-col items-center sm:col-span-10 sm:-col-end-2 md:col-span-4 md:-col-end-2 md:w-[calc(100%+20px)] md:-translate-x-5">
         <FullWidthTypo className="font-safiro-regular text-blue">CONTACT</FullWidthTypo>
         <ContactForm className="w-full shrink-0" />
